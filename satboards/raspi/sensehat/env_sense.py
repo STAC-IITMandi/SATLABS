70a0cc786c25fbba4f52d6489c7aa5e3ca3ffbb0
abhijeetmanhas <abhijeetmanhas720@gmail.com>
2019-07-05 23:47:06 +0530

    sensing environment

---
+++ satboards/raspi/sensehat/env_sense.py
@@ -0,0 +1,11 @@
+from sense_hat import SenseHat
+import time
+from datetime import datetime
+import csv
+sense = SenseHat()
+with open('env_records.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    data_writer.writerow(['datetime','temp','pres','hum'])
+    while(True):
+        data_writer.writerow([datetime.now(), sense.get_temperature(), sense.get_humidity(), sense.get_pressure()])
+        time.sleep(1)
